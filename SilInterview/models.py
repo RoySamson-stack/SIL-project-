50bdb3e05ec8114542ebc9961ff3bc0f464808e5
RoySamson-stack <you@example.com>
2024-05-27 23:11:20 +0300

    creating the models to be used for the project

--- SilInterview/models.py
+++ SilInterview/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, unique=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Order(mdoels.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    item = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.customer.name})'
