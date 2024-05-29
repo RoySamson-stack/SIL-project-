4a53f757233107fb79055ff0a36d4c4337e97956
RoySamson-stack <you@example.com>
2024-05-29 22:40:02 +0300

    fixing the error for africastalking and making it work after testing

--- SilInterview/sms.py
+++ SilInterview/sms.py
@@ -1,21 +1,17 @@
 import africastalking
 
-africastalking.initialize(
-    username='sandbox',
-    api_key='e9807969fb8d58d9e83180349935ae128224f8fc98b0b0088d0a2783f3c4a8d8'
+# Replace these with your Africa's Talking credentials
+username = "sandbox"
+api_key = "e9807969fb8d58d9e83180349935ae128224f8fc98b0b0088d0a2783f3c4a8d8"
 
-)
+# Initialize the SDK
+africastalking.initialize(username, api_key)
 
+# Get the SMS service
 sms = africastalking.SMS
 
-response = sms.send("Hello Message", ["+254702683107"])
-print(response)
-
-
-def on_finish(error, response):
-    if error is not None:
-        raise error 
-    print(response)    
-
-
-sms.send("Hello Message", ["+254702683107"], callback=on_finish)    
+try:
+    response = sms.send("Hello Message", ["+254702683107"])
+    print(response)
+except africastalking.AfricasTalkingException as e:
+    print(f"Encountered an error while sending: {str(e)}")
